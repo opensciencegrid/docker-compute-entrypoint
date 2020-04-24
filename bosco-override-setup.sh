0c38fd7866432fd79164a7df1a025f8943d0b553
Brian Lin <blin@cs.wisc.edu>
2020-04-24 10:53:27 -0500

    Allow hyphens in git usernames
    Co-Authored-By: edquist <edquist@users.noreply.github.com>

--- bosco-override-setup.sh
+++ bosco-override-setup.sh
@@ -21,7 +21,7 @@ GIT_ENDPOINT=$1
 RESOURCE_NAME=$2
 
 # pre-scan the Git repo's host key
-if [[ $GIT_ENDPOINT =~ ^[A-Za-z0-9]+@ ]]; then
+if [[ $GIT_ENDPOINT =~ ^[-A-Za-z0-9]+@ ]]; then
     SSH_ENDPOINT=${GIT_ENDPOINT%%:*}
     ssh-keyscan ${SSH_ENDPOINT##*@}
 fi
