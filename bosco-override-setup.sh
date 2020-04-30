491115c61455d9ed56361993ee965cb4706eb6cd
Brian Lin <brianhlin@gmail.com>
2020-04-30 11:46:41 -0500

    Store the Git host keys

--- bosco-override-setup.sh
+++ bosco-override-setup.sh
@@ -22,7 +22,7 @@ RESOURCE_NAME=$2
 
 # pre-scan the Git repo's host key
 if [[ $GIT_ENDPOINT =~ ^[A-Za-z0-9_-]+@([^:]+): ]]; then
-    ssh-keyscan "${BASH_REMATCH[1]}"
+    ssh-keyscan "${BASH_REMATCH[1]}" >> ~/.ssh/known_hosts
 fi
 
 REPO_DIR=$(mktemp -d)
