42648fba17edbbf8c3a9c3d4e5575b340cd25947
Carl Edquist <edquist@cs.wisc.edu>
2021-05-10 16:40:03 -0500

    remove 99_gratia.conf at container startup (SOFTWARE-4596)

--- base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
+++ base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 /usr/local/bin/configure-nonroot-gratia.py /etc/gratia/htcondor-ce/ProbeConfig
+
+# if this file exists, it contains the wrong value for PER_JOB_HISTORY_DIR
+rm -f /etc/condor-ce/config.d/99_gratia.conf
+
