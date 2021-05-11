2c394d29a662b65877d042bfa0bfea80f11516f0
Carl Edquist <edquist@cs.wisc.edu>
2021-05-11 11:45:50 -0500

    ensure condor-ce DataDir exists (SOFTWARE-4596)
    and condor owned, with expected permissions.
    
    This is taken care of by the rpm packaging in OSG 3.6 .

--- base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
+++ base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
@@ -2,6 +2,11 @@
 
 /usr/local/bin/configure-nonroot-gratia.py /etc/gratia/htcondor-ce/ProbeConfig
 
+# fixups for OSG 3.5 gratia-probe; not needed in OSG 3.6
+mkdir -p /var/lib/condor-ce/gratia/data
+chown condor:condor /var/lib/condor-ce/gratia/data
+chmod 1777 /var/lib/condor-ce/gratia/data
+
 # if this file exists, it contains the wrong value for PER_JOB_HISTORY_DIR
 rm -f /etc/condor-ce/config.d/99_gratia.conf
 
