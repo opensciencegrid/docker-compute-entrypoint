668bcf0bcc0c681df54da00aa5d50968a5e8aed8
Carl Edquist <edquist@cs.wisc.edu>
2021-05-13 17:43:37 -0500

    more directory tweaks (SOFTWARE-4596)
    - use new /var/lib/condor-ce/history dir for PER_JOB_HISTORY_DIR &
      CondorCEHistoryFolder
    
    - DataFolder is not for history dir
    
    - WorkingFolder gets its own gratia/tmp suffix
    
    - 1777 perms not needed for history dir since owned by condor

--- base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
+++ base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
@@ -2,10 +2,9 @@
 
 /usr/local/bin/configure-nonroot-gratia.py /etc/gratia/htcondor-ce/ProbeConfig
 
-# fixups for OSG 3.5 gratia-probe; not needed in OSG 3.6
-mkdir -p /var/lib/condor-ce/gratia/data
-chown condor:condor /var/lib/condor-ce/gratia/data
-chmod 1777 /var/lib/condor-ce/gratia/data
+# fixups for OSG 3.5 gratia-probe; will not be needed in OSG 3.6
+mkdir -p /var/lib/condor-ce/{history,gratia/{data,tmp}}
+chown -R condor:condor /var/lib/condor-ce/{history,gratia}
 
 # if this file exists, it contains the wrong value for PER_JOB_HISTORY_DIR
 rm -f /etc/condor-ce/config.d/99_gratia.conf
--- base/usr/local/bin/configure-nonroot-gratia.py
+++ base/usr/local/bin/configure-nonroot-gratia.py
@@ -28,9 +28,9 @@ def main():
     probe_config = probe_et.getroot()
 
     probe_config.attrib['Lockfile'] = '/var/lock/condor-ce/gratia.lock'
-    probe_config.attrib['WorkingFolder'] = '/var/lib/condor-ce/'
+    probe_config.attrib['WorkingFolder'] = '/var/lib/condor-ce/gratia/tmp/'
     probe_config.attrib['DataFolder'] = '/var/lib/condor-ce/gratia/data/'
-    probe_config.attrib['CondorCEHistoryFolder'] = '/var/lib/condor-ce/gratia/data/'
+    probe_config.attrib['CondorCEHistoryFolder'] = '/var/lib/condor-ce/history/'
     probe_config.attrib['LogFolder'] = '/var/log/condor-ce/'
 
     with open(config_path, 'w') as config_file:
