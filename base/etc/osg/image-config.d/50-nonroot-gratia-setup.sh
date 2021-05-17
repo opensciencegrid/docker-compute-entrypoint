41f5645f2c3ff63d7134224cf942200bdcd655eb
Carl Edquist <edquist@cs.wisc.edu>
2021-05-17 16:00:04 -0500

    nevermind about /var/lib/condor-ce/history
    Per Derek, we want PER_JOB_HISTORY_DIR to match DataFolder,
    and for the hosted-ce we can leave CondorCEHistoryFolder unset.
    
    Well, it turns out that you can't really unset it, because since
    SOFTWARE-2463, there is always a default for it - both in the
    packaged ProbeConfig and the gratia common code.  So anyway for
    now we'll just leave it at that default.

--- base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
+++ base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
@@ -3,8 +3,8 @@
 /usr/local/bin/configure-nonroot-gratia.py /etc/gratia/htcondor-ce/ProbeConfig
 
 # fixups for OSG 3.5 gratia-probe; will not be needed in OSG 3.6
-mkdir -p /var/lib/condor-ce/{history,gratia/{data,tmp}}
-chown -R condor:condor /var/lib/condor-ce/{history,gratia}
+mkdir -p /var/lib/condor-ce/gratia/{data,tmp}
+chown -R condor:condor /var/lib/condor-ce/gratia
 
 # if this file exists, it contains the wrong value for PER_JOB_HISTORY_DIR
 rm -f /etc/condor-ce/config.d/99_gratia.conf
