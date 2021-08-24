fd5c346301ca471879be712aaecfd91e6c951c17
Carl Edquist <edquist@cs.wisc.edu>
2021-08-24 16:11:53 -0500

    move gratia logs under gratia subdir (SOFTWARE-4596)

--- base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
+++ base/etc/osg/image-config.d/50-nonroot-gratia-setup.sh
@@ -4,7 +4,8 @@
 
 # fixups for OSG 3.5 gratia-probe; will not be needed in OSG 3.6
 mkdir -p /var/lib/condor-ce/gratia/{data,tmp}
-chown -R condor:condor /var/lib/condor-ce/gratia
+mkdir -p /var/log/condor-ce/gratia
+chown -R condor:condor /var/{lib,log}/condor-ce/gratia
 
 # if this file exists, it contains the wrong value for PER_JOB_HISTORY_DIR
 rm -f /etc/condor-ce/config.d/99_gratia.conf
